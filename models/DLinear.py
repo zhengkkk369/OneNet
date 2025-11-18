6f51bef2b66d211f95ece62cbac7a597538261e1
zhengkkk369 <74813576+zhengkkk369@users.noreply.github.com>
2025-11-18 21:35:17 +0800

    Move DLinear plugin handling into experiment

--- models/DLinear.py
+++ models/DLinear.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-from plugin.Plugin.model import Plugin
 
 class moving_avg(nn.Module):
     """
@@ -37,6 +36,7 @@ class series_decomp(nn.Module):
         res = x - moving_mean
         return res, moving_mean
 
+
 class Model(nn.Module):
     """
     Decomposition-Linear
@@ -45,9 +45,6 @@ class Model(nn.Module):
         super(Model, self).__init__()
         self.seq_len = configs.seq_len
         self.pred_len = configs.pred_len
-        self.flag = getattr(configs, 'flag', None)
-        if self.flag == 'Plugin':
-            self.plugin = Plugin(configs, configs.c_out)
 
         # Decompsition Kernel Size
         kernel_size = 25
@@ -58,18 +55,18 @@ class Model(nn.Module):
         if self.individual:
             self.Linear_Seasonal = nn.ModuleList()
             self.Linear_Trend = nn.ModuleList()
-            
+
             for i in range(self.channels):
-                self.Linear_Seasonal.append(nn.Linear(self.seq_len,self.pred_len))
-                self.Linear_Trend.append(nn.Linear(self.seq_len,self.pred_len))
+                self.Linear_Seasonal.append(nn.Linear(self.seq_len, self.pred_len))
+                self.Linear_Trend.append(nn.Linear(self.seq_len, self.pred_len))
 
                 # Use this two lines if you want to visualize the weights
                 # self.Linear_Seasonal[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
                 # self.Linear_Trend[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
         else:
-            self.Linear_Seasonal = nn.Linear(self.seq_len,self.pred_len)
-            self.Linear_Trend = nn.Linear(self.seq_len,self.pred_len)
-            
+            self.Linear_Seasonal = nn.Linear(self.seq_len, self.pred_len)
+            self.Linear_Trend = nn.Linear(self.seq_len, self.pred_len)
+
             # Use this two lines if you want to visualize the weights
             # self.Linear_Seasonal.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
             # self.Linear_Trend.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
@@ -78,25 +75,22 @@ class Model(nn.Module):
         # x: [Batch, Input length, Channel]
         x_enc = x
         seasonal_init, trend_init = self.decompsition(x_enc)
-        seasonal_init, trend_init = seasonal_init.permute(0,2,1), trend_init.permute(0,2,1)
+        seasonal_init, trend_init = seasonal_init.permute(0, 2, 1), trend_init.permute(0, 2, 1)
         if self.individual:
-            seasonal_output = torch.zeros([seasonal_init.size(0),seasonal_init.size(1),self.pred_len],dtype=seasonal_init.dtype).to(seasonal_init.device)
-            trend_output = torch.zeros([trend_init.size(0),trend_init.size(1),self.pred_len],dtype=trend_init.dtype).to(trend_init.device)
+            seasonal_output = torch.zeros(
+                [seasonal_init.size(0), seasonal_init.size(1), self.pred_len], dtype=seasonal_init.dtype
+            ).to(seasonal_init.device)
+            trend_output = torch.zeros(
+                [trend_init.size(0), trend_init.size(1), self.pred_len], dtype=trend_init.dtype
+            ).to(trend_init.device)
             for i in range(self.channels):
-                seasonal_output[:,i,:] = self.Linear_Seasonal[i](seasonal_init[:,i,:])
-                trend_output[:,i,:] = self.Linear_Trend[i](trend_init[:,i,:])
+                seasonal_output[:, i, :] = self.Linear_Seasonal[i](seasonal_init[:, i, :])
+                trend_output[:, i, :] = self.Linear_Trend[i](trend_init[:, i, :])
         else:
             seasonal_output = self.Linear_Seasonal(seasonal_init)
             trend_output = self.Linear_Trend(trend_init)
 
         x = seasonal_output + trend_output
-        pred = x.permute(0,2,1) # to [Batch, Output length, Channel]
-
-        if self.flag == 'Plugin':
-            x_enc_copy = x_enc.clone()
-            x_mark_enc_copy = None if x_mark_enc is None else x_mark_enc.clone()
-            x_mark_dec_copy = None if x_mark_dec is None else x_mark_dec.clone()
-            if x_mark_enc_copy is not None and x_mark_dec_copy is not None:
-                pred = self.plugin(x_enc_copy, x_mark_enc_copy, pred, x_mark_dec_copy[:, -self.pred_len:, :])
+        pred = x.permute(0, 2, 1)  # to [Batch, Output length, Channel]
 
         return pred
